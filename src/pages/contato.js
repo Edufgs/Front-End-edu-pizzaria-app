666c7af9638fedf173714ed23b1b3adc29c4391f
Edufgs <edu.fgs14@gmail.com>
2022-09-09 17:23:26 -0400

    initial setting

---
+++ src/pages/contato.js
@@ -0,0 +1,14 @@
+
+//Criação de componente sempre a primeira letra é maiuscula
+/**
+ * Para o Nextjs reconhecer como rota,
+ * então é colocado export default e 
+ * automaticamente é criado a rota para esse componente
+ */
+export default function Contado(){
+    return(
+        <div>
+            <h1>Pagina Contatos</h1>
+        </div>
+    )
+}
