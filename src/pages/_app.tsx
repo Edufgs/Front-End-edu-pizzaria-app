f0e26f1a18d9340e5e17629b7b8f87bff87c668b
Edufgs <edu.fgs14@gmail.com>
2022-09-09 18:10:17 -0400

    configuring style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
+import '../../styles/globals.scss'
 //Fazer tipagem
 import {AppProps} from 'next/app';
+
+/**
+ * Esse é o componente global que é executado
+ * 
+ */
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
