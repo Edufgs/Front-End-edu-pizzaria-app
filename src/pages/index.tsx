06779ff02bc965566ecfb58e567c33f7de5a92c2
Edufgs <edu.fgs14@gmail.com>
2022-09-17 15:05:58 -0400

    add document and fix color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-
+import Head from 'next/head'
 
 export default function Home() {
   return (
+    //Tag sem nome sem nada
+    <>    
+    <Head>
+      {/* Titulo que fica em cima da aba */}  
+      <title>Edu Pizza - Faça seu login</title>
+    </Head>
     <div>
-      <h1>Edu Pizza</h1>
+      <h1>Edu Pizzaria</h1>
     </div>
+    </>
   )
 }
